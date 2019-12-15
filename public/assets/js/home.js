9f5f3c2560ff6fc3ea700f7af03213d2babaa712
Alper-Enes-DURU <enesduru@gmail.com>
2019-12-15 00:48:52 +0000

    Lesson 16: Used jQuery $.ajax method to make AJAX requests.

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,6 +1,9 @@
 const $setLogin = $(`#login`);
 const $setSignUp = $(`#signup`);
 const $submitButton = $('#submit');
+const $emailInput = $(`#email`);
+const $passwordInput = $(`#password`);
+const $message = $(`#message`);
 
 let authSetting = 'login';
 
@@ -18,5 +21,46 @@ function setAuth(setting) {
   }
 }
 
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  let email = $emailInput.val().trim();
+  let password = $passwordInput.val().trim();
+
+  if (!email || !password) {
+    displayMessage('Email and password fields cannot be blank.', 'danger');
+    return;
+  }
+
+  console.log(
+    `Email: ${email} Password: ${password} AuthSetting: ${authSetting}`
+  );
+
+  $emailInput.val('');
+  $passwordInput.val('');
+
+  authenticateUser(email, password);
+}
+
+function displayMessage(message, type) {
+  $message.text(message).attr('class', type);
+}
+
+function authenticateUser(email, password) {
+  $.ajax({
+    url: '/' + authSetting,
+    data: {
+      user: {
+        email: email,
+        password: password
+      }
+    },
+    method: 'POST'
+  }).then(function(data){
+    console.log(data);
+  });
+}
+
 $setLogin.on('click', setAuth.bind(null, 'login'));
 $setSignUp.on('click', setAuth.bind(null, 'signup'));
+$submitButton.on('click', handleFormSubmit);
